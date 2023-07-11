625b6b82be748a32fa909a61211376a37bb62c5a
Rose <trshpuppy@proton.me>
2023-07-11 18:52:04 -0400

    its whack

--- main.py
+++ main.py
@@ -2,41 +2,75 @@
 from twitchio.ext import commands
 import json
 
-class Bot(commands.Bot):
-    
-    def __init__(self):
-        # Load api/ oauth data from data/api.json:
-        with open('data/api.json') as f:
-            api = json.load(f)
 
-        bot_username = api['Bot']['BOT_USERNAME']
-        prefix = api['Bot']['PREFIX']
-        # target_twitch_channel = [api['Bot']['CHANNEL']]
 
-        super().__init__(# 'irc_token' evidentally has to be 'token' instead
-                         token=api['OA_TOKEN'], 
-                         client_id=api['CLIENT_ID'], 
-                         nick=bot_username, 
-                         prefix=prefix,
-                         # this has to be an array but setting `target_twitch_channel`
-                         # on line 14 to = [api['Bot]['CHANNEL]] didn't work.
-                         # Channel name hard coded for now :)
-                         initial_channels=['trshpuppy'])
+# Load api/ oauth data from data/api.json:
+with open('data/api.json') as f:
+    api = json.load(f)
 
-    async def event_ready(self):
-        print(f'Ready | {self.nick}')
+bot_username = api['Bot']['BOT_USERNAME']
+prefix = api['Bot']['PREFIX']
 
-    async def event_message(self, message):
-        print(message.content)
-        await self.handle_commands(message)
+bot = commands.Bot(
+    # 'irc_token' evidentally has to be 'token' instead
+    token=api['OA_TOKEN'], 
+    client_id=api['CLIENT_ID'], 
+    nick=bot_username, 
+    prefix=prefix,
+    # this has to be an array but setting `target_twitch_channel`
+    # on line 14 to = [api['Bot]['CHANNEL]] didn't work.
+    # Channel name hard coded for now :)
+    initial_channels=['trshpuppy']
+    )
 
-    @commands.command(name='hello')
-    async def my_command(self, ctx):
-        await ctx.send('Hello World!')
+#bot = Bot()
+@bot.event()
+async def event_ready():
+    print(f"Ready @{bot_username}")
 
-    @commands.command(name='bake')
-    async def bake_command(self, ctx):
-        await ctx.send('bake')
+@bot.event()
+async def event_message(ctx):
+    if ctx.echo:
+        print(f"event_message: ")
+    await ctx.channel.send(f"hello {ctx.author.name}")
 
-bot = Bot()
+
+
 bot.run()
+
+# class Bot(commands.Bot):
+    
+    # def __init__(self):
+    #     # Load api/ oauth data from data/api.json:
+    #     with open('data/api.json') as f:
+    #         api = json.load(f)
+
+    #     bot_username = api['Bot']['BOT_USERNAME']
+    #     prefix = api['Bot']['PREFIX']
+    #     # target_twitch_channel = [api['Bot']['CHANNEL']]
+
+    #     super().__init__(# 'irc_token' evidentally has to be 'token' instead
+    #                      token=api['OA_TOKEN'], 
+    #                      client_id=api['CLIENT_ID'], 
+    #                      nick=bot_username, 
+    #                      prefix=prefix,
+    #                      # this has to be an array but setting `target_twitch_channel`
+    #                      # on line 14 to = [api['Bot]['CHANNEL]] didn't work.
+    #                      # Channel name hard coded for now :)
+    #                      initial_channels=['trshpuppy'])
+
+    # async def event_ready(self):
+    #     print(f'Ready | {self.nick}')
+
+    # async def event_message(self, message):
+    #     print(message.content)
+    #     await self.handle_commands(message)
+
+    # @commands.command(name='hello')
+    # async def hello(self, ctx):
+    #     print(commands.hello.message.author)
+    #     await ctx.send('Hello World!')
+
+    # @commands.command(name='bake')
+    # async def bake(self, ctx):
+    #     await ctx.send('bake')
