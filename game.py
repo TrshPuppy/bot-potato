edf41b357e9a1408ef270a0a2cf078dbb233d2ad
Leonard1415 <113802071+Leonard1415@users.noreply.github.com>
2023-07-15 10:56:33 +0200

    Update game.py

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-import time
+from time import sleep
 import threading
 import random
 import json
@@ -6,49 +6,75 @@ import json
 # Some dead kittens, I MEAN... globals...
 current_game = None
 DEFAULT_MIN_PASSES = 2
-DEFAULT_GAME_TIMEOUT = 30
+DEFAULT_TIME_TO_PASS = 30
+DEFAULT_GAME_TIME = 5 * 60
 
 
 # need some default parameters for game:
 class Game:
     # use default values for ctor parameters
-    def __init__(self, min_passes_between=None, game_timeout=None):
-        self.increment = 1  # loop timer, check for timeouts every second
-        self.next_loop = int(time.time())
-        self.done = False
+    def __init__(self,
+                 min_passes=DEFAULT_MIN_PASSES,
+                 time_to_pass=DEFAULT_TIME_TO_PASS,
+                 game_time=DEFAULT_GAME_TIME):
 
-        self.start_time = 0
+        self.min_passes = min_passes
+        self.time_to_pass = time_to_pass
+        self.game_time = game_time
         self.active = False
         self.num_passes = 0
         self.current_player = None
         self.active_players = []
-
-        if min_passes_between == None:
-            self.min_passes_between = DEFAULT_MIN_PASSES
-        else:
-            self.minPasses_between = min_passes_between
-        if game_timeout == None:
-            self.game_timeout = DEFAULT_GAME_TIMEOUT
-        else:
-            self.game_timeout = game_timeout
-
-        # note that player_timeout is not here because we now add players before starting the game
-        self.dropped = False
+        self.game_timer = 0
+        self.pass_timer = 0
 
     def start_game(self):
-        self.start_time = int(time.time())  # format is Unix, ex: 162717366
         self.active = True
-        if self.current_player == None:
-            random_player_indx = random.randint(0, len(current_game.active_players) - 1)
-            current_game.current_player = current_game.active_players[
+        if self.current_player is None:
+            random_player_indx = random.randint(0, len(self.active_players) - 1)
+            self.current_player = self.active_players[
                 random_player_indx
             ]
 
-        self._run()
+        self.game_thread = threading.Thread(target=self.game_loop)
+        self.game_thread.start()
 
-    def end_game(self, et):
+    def game_loop(self):
+        while self.active:
+            if self.pass_timer > self.time_to_pass:
+                print(f"{self.current_player.username} failed to pass the potato in time!")
+                self.end_game(win=False)
+            if self.game_timer > self.game_time:
+                self.end_game(win=True)
+            sleep(1)
+            self.game_timer += 1
+            self.pass_timer += 1
+
+    def pass_potato(self, to_player):
+        # Validate to_player: exists in game, etc.
+        if to_player not in self.active_players:
+            print(f"{to_player.username} is not in the game.")
+            return
+            
+        # Check passes
+        if self.num_passes - to_player.last_passed < self.min_passes_between:
+            print(f"{to_player.username} already had it too recently.")
+            return
+
+        # update game and player states, e.g. time received, last passed, num_passes, current player...
+        self.num_passes += 1
+        self.pass_timer = 0
+        to_player.receive_potato(
+            self.num_passes)
+        self.current_player = to_player
+        
+
+    def end_game(self, win):
         self.active = False
-        # self.end_time = int(int(time.time())) # format is Unix, ex: 162717366
+        if win:
+            print('Players won the game')
+        else:
+            print('Players lost the game')
 
     def check_for_player(self, p):
         print(f"new player id is {p.id}")
@@ -68,45 +94,10 @@ class Game:
         # do we reset game state or destroy this one and always construct a new one?
         return
 
-    # Game needs to _run a loop for game timer so import threading
-    def _run(self):
-        self.next_loop += self.increment
-        # check for player and game timeouts
-        curr_time = int(time.time())
-
-        with open("data/stats.json") as s:
-            game_stats = json.load(s)
-
-        print(f"length of active_players in game state is {len(s.active_players)}")
-        # print(f"current_player= {self.current_player.username}")
-
-        # global timeout?
-        # if (curr_time - self.start_time) > self.game_timeout:
-        #     print(f"global timeout")
-        #     # resolve
-        # elif (
-        #     curr_time - self.current_player.time_received
-        # ) > self.current_player.player_timeout:
-        #     print(f"player timeout")
-        #     self.dropped = True
-        # resolve
-        # player timeout?
-
-        # check potato player to see if they've timed out
-
-        # update self.dropped if needed
-        # self.active = False
-        if not self.done:
-            threading.Timer(self.next_loop - int(time.time()), self._run).start()
-
-    def stop(self):
-        self.done = True
-
 
 def is_game_active():
-    global current_game
-    print(f"HERY!!! the game is {current_game}")
-    if current_game == None:
+    print(f"HEY!!! the game is {current_game}")
+    if current_game is None:
         return False
     return current_game.active
 
@@ -149,20 +140,6 @@ async def try_to_add_player(p, ctx):
 
     current_game.player_join_game(p)
     return True
-
-
-def pass_potato():
-    # validate toPlayeri: exists in game, etc
-
-    # check toPlayer lastPassed
-    if self.num_passes - toPlayer.last_passed < self.min_passes_between:
-        print(f"toPlayer already had it too recently")
-        return
-
-    # update game and player states, e.g. time received, last passed, num_passes, current player...
-
-    # TODO: decrease passing player timeout
-    return 0
 
 
 def print_players(ctx):
