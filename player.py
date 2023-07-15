0a0ae1d7cbe4625afabf1cceba060e67ff027c48
turingmoonyatch <139160947+turingmoonyatch@users.noreply.github.com>
2023-07-15 01:31:48 -0400

    Update player.py with comments

--- player.py
+++ player.py
@@ -9,20 +9,24 @@ class Player:
         self.id = i
         self.username = u
         self.last_passed = (
-            0  # of passes in the game since the player last had the potato
+            0  # global turn number where this player last received the potato.
         )
         self.time_received = 0
         self.current_timeout = 0  # the decreasing ammount of time player has to pass
         # self.hasPotato = False  # This might make it easier for game to check players?
 
-    def receive_potato(self, po):
+    def receive_potato(self, po, turn):
+        # time_received is measured in Unix time
+        # last passed is measured in number of turns, not time
         self.time_received = int(time.time())
-        self.last_passed = int(
-            time.time()
-        )  # set when they receive potato (vs when they pass it?)
-        self.hasPotato = True
-        self.current_timeout = DEFAULT_TIME_TO_PASS
+        self.last_passed = turn
+        # set when they receive potato (vs when they pass it?)
+        # self.hasPotato = True
+        self.current_timeout = DEFAULT_TIME_TO_PASS.  # fixed time to pass, we can make this decay by not here.
+        # the passing player's time decays, not the receiving player
 
+    # the player doesn't need to check countdown. 
+    # the game loop is responsible for checking timeouts
     def check_countdown(self):
         time_now = int(time.time())
         time_left_to_pass = self.current_timeout - time_now
